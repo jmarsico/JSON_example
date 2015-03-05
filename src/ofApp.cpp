bf67a7c052baa4036ef22100c1c38df882fcf016
Jake <jmarsico@gmail.com>
2015-03-05 19:43:36 +0000

    added getData()

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -15,44 +15,50 @@ void ofApp::update(){
     
     //every two minutes, request an update
     int timeMillis = (ofGetElapsedTimeMillis() % (120 * 1000));
+    
     if( timeMillis > 0 && timeMillis < 10){
-        ofLog() << "retreiving data at " << ofGetTimestampString();
-        
-        //get the response and the status of the GET request
-        ofxHttpResponse response = httpUtils.getUrl(action_url);
-        int status = response.status;
-        
-        //let us know what the status and reason is
-        ofLog() << "status: " << status;
-        ofLog() << "reason: " << response.reasonForStatus;
-        
-        //if success status:
-        if(status == 200)
-        {
-            //see if we can parse the data
-            bool success = json.parse(response.responseBody);
-            
-            //if we can parse, print it out
-            if(success)
-            {
-                ofLog() << json.getRawString();
-            } else
-            {
-                ofLog() << "could not parse";
-            }
-           
-            //get the "value" parameter and read it as a float
-            value = json["Value"].asFloat();
-            
-            //tell us what it is
-            ofLog() << "value: " << value;
-        }
-
+        getData();
     }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::getData(){
+    ofLog() << "retreiving data at " << ofGetTimestampString();
+        
+    //get the response and the status of the GET request
+    ofxHttpResponse response = httpUtils.getUrl(action_url);
+    int status = response.status;
+    
+    //let us know what the status and reason is
+    ofLog() << "status: " << status;
+    ofLog() << "reason: " << response.reasonForStatus;
+    
+    //if success status:
+    if(status == 200)
+    {
+        //see if we can parse the data
+        bool success = json.parse(response.responseBody);
+        
+        //if we can parse, print it out
+        if(success)
+        {
+            ofLog() << json.getRawString();
+        } else
+        {
+            ofLog() << "could not parse";
+        }
+       
+        //get the "value" parameter and read it as a float
+        value = json["Value"].asFloat();
+        
+        //tell us what it is
+        ofLog() << "value: " << value;
+    }
 
 }
 
